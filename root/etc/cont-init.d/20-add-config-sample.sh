4a74a3b4e613c46a194fac2c34a5151694b5328b
Tom <TommyE123@users.noreply.github.com>
2018-06-16 12:46:45 +0100

    Update 20-add-config-sample.sh

--- root/etc/cont-init.d/20-add-config-sample.sh
+++ root/etc/cont-init.d/20-add-config-sample.sh
@@ -26,14 +26,14 @@ fi
 
 if [ -n "$PACKT_DOWNLOAD_FORMATS" ]; then
     echo "ENV PACKT_DOWNLOAD_FORMATS provided as '$PACKT_DOWNLOAD_FORMATS'"
-    sed -i s/download_formats:.*/download_formats: $PACKT_DOWNLOAD_FORMATS/ $CFG
+    sed -i s/download_formats:.*/download_formats:\ $PACKT_DOWNLOAD_FORMATS/ $CFG
 else
     echo "ENV PACKT_DOWNLOAD_FORMATS not set"
 fi
 
 if [ -n "$PACKT_DOWNLOAD_BOOK_TITLES" ]; then
     echo "ENV PACKT_DOWNLOAD_BOOK_TITLES provided as '$PACKT_DOWNLOAD_BOOK_TITLES'"
-    sed -i s/download_book_titles:.*/download_book_titles: $PACKT_DOWNLOAD_BOOK_TITLES/ $CFG
+    sed -i s/download_book_titles:.*/download_book_titles:\ $PACKT_DOWNLOAD_BOOK_TITLES/ $CFG
 else
     echo "ENV PACKT_DOWNLOAD_BOOK_TITLES not set"
     sed -i s/download_book_titles:.*/download_book_titles:/ $CFG
@@ -41,13 +41,13 @@ fi
 
 if [ -n "$PACKT_ANTICAPTCHA_DATA" ]; then
     echo "ENV PACKT_ANTICAPTCHA_DATA provided"
-    sed -i s/key:.*/key: $PACKT_ANTICAPTCHA_DATA/ $CFG
+    sed -i s/key:.*/key:\ $PACKT_ANTICAPTCHA_DATA/ $CFG
 else
     echo "ENV PACKT_ANTICAPTCHA_DATA not set"
 fi
 
 echo "Replacing path with /data"
-sed -i "s/downloadFolderPath:.*/downloadFolderPath: \/data/" $CFG
+sed -i "s/downloadFolderPath:.*/downloadFolderPath:\ \/data/" $CFG
 
 echo "Set logfile path to /data"
-sed -i "s/ebook_extra_info_log_file_path:.*/ebook_extra_info_log_file_path: \/data\/eBookMetadata.log/" $CFG
+sed -i "s/ebook_extra_info_log_file_path:.*/ebook_extra_info_log_file_path:\ \/data\/eBookMetadata.log/" $CFG
