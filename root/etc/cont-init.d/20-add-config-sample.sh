700eb71f1a5479693eca6244a9a7a271365d40a3
Tom <TommyE123@users.noreply.github.com>
2018-06-17 10:25:40 +0100

    Update 20-add-config-sample.sh

--- root/etc/cont-init.d/20-add-config-sample.sh
+++ root/etc/cont-init.d/20-add-config-sample.sh
@@ -5,49 +5,48 @@ CFG=/config/configFile.cfg
 
 # This copies the sample config in place if not one present
 if [ ! -f $CFG ]; then
-    echo "No config file, providing sample"
+    echo 'No config file, providing sample'
     cp /root/Packt-Publishing-Free-Learning/src/configFileTemplate.cfg $CFG
-    chown abc:abc $CFG
 fi
 # If environment arguments have been provided, switch the values in the sample config to these
 if [ -n "$PACKT_EMAIL" ]; then
-    echo "ENV PACKTEMAIL provided"
+    echo 'ENV PACKTEMAIL provided'
     sed -i s/email=.*/email=$PACKT_EMAIL/ $CFG
 else
-    echo "ENV PACKTEMAIL not set"
+    echo 'ENV PACKTEMAIL not set'
 fi
 
 if [ -n "$PACKT_PASSWORD" ]; then
-    echo "ENV PACKTPASSWORD provided"
+    echo 'ENV PACKTPASSWORD provided'
     sed -i s/password=.*/password=$PACKT_PASSWORD/ $CFG
 else
-    echo "ENV PACKTEMAIL not set"
+    echo 'ENV PACKTEMAIL not set'
 fi
 
 if [ -n "$PACKT_DOWNLOAD_FORMATS" ]; then
     echo "ENV PACKT_DOWNLOAD_FORMATS provided as '$PACKT_DOWNLOAD_FORMATS'"
     sed -i s/download_formats:.*/download_formats:\ $PACKT_DOWNLOAD_FORMATS/ $CFG
 else
-    echo "ENV PACKT_DOWNLOAD_FORMATS not set"
+    echo 'ENV PACKT_DOWNLOAD_FORMATS not set'
 fi
 
 if [ -n "$PACKT_DOWNLOAD_BOOK_TITLES" ]; then
     echo "ENV PACKT_DOWNLOAD_BOOK_TITLES provided as '$PACKT_DOWNLOAD_BOOK_TITLES'"
     sed -i s/download_book_titles:.*/download_book_titles:\ $PACKT_DOWNLOAD_BOOK_TITLES/ $CFG
 else
-    echo "ENV PACKT_DOWNLOAD_BOOK_TITLES not set"
+    echo 'ENV PACKT_DOWNLOAD_BOOK_TITLES not set'
     sed -i s/download_book_titles:.*/download_book_titles:/ $CFG
 fi
 
 if [ -n "$PACKT_ANTICAPTCHA_DATA" ]; then
-    echo "ENV PACKT_ANTICAPTCHA_DATA provided"
+    echo 'ENV PACKT_ANTICAPTCHA_DATA provided'
     sed -i s/key:.*/key:\ $PACKT_ANTICAPTCHA_DATA/ $CFG
 else
-    echo "ENV PACKT_ANTICAPTCHA_DATA not set"
+    echo 'ENV PACKT_ANTICAPTCHA_DATA not set'
 fi
 
-echo "Replacing path with /data"
-sed -i s/download_Folder_Path:.*/download_Folder_Path:\ \/data/ $CFG
+echo 'Replacing path with /data'
+sed -i s@download_folder_path:.*@download_folder_path:\ \/data@ $CFG
 
-echo "Set logfile path to /data"
-sed -i s/ebook_extra_info_log_file_path:.*/ebook_extra_info_log_file_path:\ \/data\/eBookMetadata.log/ $CFG
+echo 'Set logfile path to /data'
+sed -i s@ebook_extra_info_log_file_path:.*@ebook_extra_info_log_file_path:\ \/data\/eBookMetadata.log@ $CFG
