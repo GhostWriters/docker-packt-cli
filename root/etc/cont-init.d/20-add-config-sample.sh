507c42f74a73809bb99b8870a617742c613f1116
Tom <tom.eley@gmail.com>
2018-06-18 21:27:09 +0100

    Shellcheck fixes

--- root/etc/cont-init.d/20-add-config-sample.sh
+++ root/etc/cont-init.d/20-add-config-sample.sh
@@ -11,28 +11,28 @@ fi
 # If environment arguments have been provided, switch the values in the sample config to these
 if [ -n "$PACKT_EMAIL" ]; then
     echo 'ENV PACKT_EMAIL provided'
-    sed -i s/email=.*/email=$PACKT_EMAIL/ $CFG
+    sed -i s/email=.*/email="$PACKT_EMAIL"/ $CFG
 else
     echo 'ENV PACKTEMAIL not set'
 fi
 
 if [ -n "$PACKT_PASSWORD" ]; then
     echo 'ENV PACKT_PASSWORD provided'
-    sed -i s/password=.*/password=$PACKT_PASSWORD/ $CFG
+    sed -i s/password=.*/password="$PACKT_PASSWORD"/ $CFG
 else
     echo 'ENV PACKTEMAIL not set'
 fi
 
 if [ -n "$PACKT_DOWNLOAD_FORMATS" ]; then
     echo "ENV PACKT_DOWNLOAD_FORMATS provided as '$PACKT_DOWNLOAD_FORMATS'"
-    sed -i s/download_formats:.*/download_formats:\ $PACKT_DOWNLOAD_FORMATS/ $CFG
+    sed -i s/download_formats:.*/download_formats:\ "$PACKT_DOWNLOAD_FORMATS"/ $CFG
 else
     echo 'ENV PACKT_DOWNLOAD_FORMATS not set'
 fi
 
 if [ -n "$PACKT_DOWNLOAD_BOOK_TITLES" ]; then
     echo "ENV PACKT_DOWNLOAD_BOOK_TITLES provided as '$PACKT_DOWNLOAD_BOOK_TITLES'"
-    sed -i s/download_book_titles:.*/download_book_titles:\ $PACKT_DOWNLOAD_BOOK_TITLES/ $CFG
+    sed -i s/download_book_titles:.*/download_book_titles:\ "$PACKT_DOWNLOAD_BOOK_TITLES"/ $CFG
 else
     echo 'ENV PACKT_DOWNLOAD_BOOK_TITLES not set'
     sed -i s/download_book_titles:.*/download_book_titles:/ $CFG
@@ -40,13 +40,13 @@ fi
 
 if [ -n "$PACKT_ANTICAPTCHA_KEY" ]; then
     echo 'ENV PACKT_ANTICAPTCHA_Key provided'
-    sed -i s/key:.*/key:\ $PACKT_ANTICAPTCHA_KEY/ $CFG
+    sed -i s/key:.*/key:\ "$PACKT_ANTICAPTCHA_KEY"/ $CFG
 else
     echo 'ENV PACKT_ANTICAPTCHA_KEY not set'
 fi
 
 echo 'Replacing path with /data'
-sed -i s@download_folder_path:.*@download_folder_path:\ \/data@ $CFG
+sed -i s@download_folder_path:.*@download_folder_path:\ \\/data@ $CFG
 
 echo 'Set logfile path to /data'
-sed -i s@ebook_extra_info_log_file_path:.*@ebook_extra_info_log_file_path:\ \/data\/eBookMetadata.log@ $CFG
+sed -i s@ebook_extra_info_log_file_path:.*@ebook_extra_info_log_file_path:\ \\/data\\/eBookMetadata.log@ $CFG
